875199f9c07771197c1ebe8d8a99d5269bde94c4
David Lopez <dwlopez91@gmail.com>
2019-09-19 22:50:06 -0500

    added code for average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,8 @@ import csv
 #intialize variables
 total_months = 0
 total_revenue = 0
+intervals = []
+
 
 # Set path for file
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
@@ -20,23 +22,33 @@ with open(csvpath, newline='') as csvfile:
     #set our ititial variables
     total_months = total_months + 1 
     total_revenue += int(row[1])
+
+    previous_month = int(row[1])
    
    
     for row in csvreader:
         total_months += 1
         total_revenue += int(row[1])
+        interval_amount = int(row[1]) - previous_month
+        intervals.append(interval_amount)
+        previous_month = int(row[1])
 
+
+
+    average_change = round(sum(intervals)/len(intervals), 2)
 
 
 print("Financial Analysis")
 print("-------------------")
 print(f"Total Months: {total_months}")
 print(f"Total Revenue: ${total_revenue}")
+print(f"Average Change: ${average_change}")
 
-output_path = os.path.join("..", "output", "new.csv")
+# # save the output file path
+# output_path = os.path.join("..", "output", "new.csv")
 
-# Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w', newline='') as csvfile:
+# # Open the file using "write" mode. Specify the variable to hold the contents
+# with open(output_path, 'w', newline='') as csvfile:
 
-    # Initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
+#     # Initialize csv.writer
+#     csvwriter = csv.writer(csvfile, delimiter=',')
