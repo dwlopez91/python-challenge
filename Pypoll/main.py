ff8afd7a32a3a1f6232a683a1808635ff8a1f198
David Lopez <dwlopez91@gmail.com>
2019-09-21 20:35:24 -0500

    trying to get output file

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -4,9 +4,9 @@ import csv
 #intialize variables
 total_votes = 0
 khan = 0
-# correy = candidate_list.count("Correy")
-# li = candidate_list.count("Li")
-# otooley = candidate_list.count("O'Tooley")
+correy = 0
+li = 0
+otooley = 0
 
 # Set path for file
 csvpath = os.path.join('..', 'Resources', 'election_data.csv')
@@ -22,13 +22,51 @@ with open(csvpath, newline='') as csvfile:
 
     total_votes = total_votes + 1
 
-
+    #iterate
     for row in csvreader:
         total_votes += 1
         if(row[2] == "Khan"):
             khan += 1
-khan_percent = round(khan/total_votes * 100)
+        elif(row[2] == "Correy"):
+            correy += 1
+        elif(row[2] == "Li"):
+            li += 1
+        elif(row[2] == "O'Tooley"):
+            otooley += 1    
+    #divide the votes per candidate by total votes to find percentage
+    khan_percent = round(khan/total_votes * 100)
+    correy_percent = round(correy/total_votes * 100)
+    li_percent = round(li/total_votes * 100)
+    otooley_percent = round(otooley/total_votes * 100)
 
-print(total_votes)
-print(khan)
-print(khan_percent)
+    
+    
+   
+    
+    
+
+
+print("Election Results")
+print(f"Total Votes: {total_votes}")
+print("--------")
+print(f"Khan: {khan_percent}% ({khan})")
+print(f"Correy: {correy_percent}% ({correy})")
+print(f"Li: {li_percent}% ({li})")
+print(f"O'Tooley: {otooley_percent}% ({otooley})")
+print("--------")
+
+
+# Specify the file to write to
+output_path = os.path.join("..", "output", "new.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row (column headers)
+    csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
+
+    # Write the second row
+    csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
